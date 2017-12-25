9eef2821ea19730fec6d9dff1cbd205a22f5e09d
Noah Halstead <nhalstead00@gmail.com>
2017-12-24 20:15:16 -0500

    Init
    Added the Code that I have gotten to work.

---
+++ src/switch.js
@@ -0,0 +1,39 @@
+/*!
+ * Switch Text JavaScript Library v1.0.1
+ * https://jquery.com/
+ *
+ * Copyright Noah Halstead and other contributors
+ * Released under the MIT license
+ *
+ * Date: 2017-12-24
+ */
+function contentSwitcher(className, timeSwitch, list){
+  this.className = className;
+  this.clockTime = timeSwitch;
+  this.items     = list;
+  this.clockHold = null;
+
+  // Start the Interval for the Switching Action
+  this.startSwitching = function(){
+    this.clockHold = setInterval( function(inClassName, inItems){
+      console.log("Switch Content for: " + inClassName);
+      var el = document.getElementsByClassName(inClassName);
+      for (var key in el) {
+        let e = el[key];
+        let n = inItems[Math.floor(Math.random()*inItems.length)];
+        e.innerHTML = n;
+      }
+    } , this.clockTime, this.className, this.items);
+  };
+
+  // Clear the setInterval
+  this.stopSwitching = function(){
+    clearInterval(this.clockHold);
+  };
+
+  this.add = function(itm){
+    this.items.push(itm);
+    this.stopSwitching();
+    this.startSwitching();
+  }
+}
